f5ddd3c6c8f13fd249cea183405a584a3f077e05
aoda <aoda.q.zhang@newegg.com>
2022-04-08 20:31:54 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   // TODO 完善远端文件过滤
   // TODO 完善流程逻辑，询问用户 具体的方向 node后端，web前端
-  // TODO config文件抽离
   // TODO 完善工程化（请求封装，proxy代理设置，路由配置，webpack扩展配置，多语言扩展）
+  //TODO 下载码云  https://github.com/mengmengzp/cli/blob/main/bin/fems-create
   /**脚手架当前支持创建的项目技术 */
   CURRENT_SUPPORT_KEY: ["vue-template", "vue3.0-template"],
   /**当前采用的git用户账号*/
