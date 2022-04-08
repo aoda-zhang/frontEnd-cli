8ebc9b023e455536a7605bea9da65d7bffb1f773
aoda <aoda.q.zhang@newegg.com>
2022-04-08 10:04:42 +0800

    初始化config配置

---
+++ config/index.js
@@ -0,0 +1,11 @@
+module.exports={
+    // TODO 完善提示信息
+    // TODO 完善远端文件过滤
+    // TODO 完善流程逻辑，询问用户 具体的方向 node后端，web前端
+    // TODO config文件抽离
+    // TODO 完善工程化（请求封装，proxy代理设置，路由配置，webpack扩展配置，多语言扩展）
+    /**脚手架当前支持创建的项目技术 */ 
+    CURRENT_SUPPORT_KEY:[ 'vue-template', 'vue3.0-template' ],
+    /**当前采用的git用户账号*/ 
+    CURRENT_GIT_USER:'aodagit',
+}
