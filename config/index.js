1da7fda05e1ec8e1fdfe9720b74177a8469d78a6
aoda <aoda.q.zhang@newegg.com>
2022-04-08 20:05:20 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // TODO 完善提示信息
   // TODO 完善远端文件过滤
   // TODO 完善流程逻辑，询问用户 具体的方向 node后端，web前端
   // TODO config文件抽离
@@ -15,8 +14,21 @@ module.exports = {
     "抱歉未能给您解决脚手架带来的问题，如需帮助，请联系我，电话 19102632826，base：成都",
   /**cli创建提示信息*/
   CLI_CREATE_HELP_MESSAGE: "创建一个新的项目",
+  /**cli初始化提示信息*/
+  CLI_INIT_HELP_MESSAGE: "项目创建初始化...",
+  /**cli创建时的技术方向提示信息*/
+  CLI_CREATE_CHOICE_HELP_MESSAGE: "您的项目主要基于那个模板来创建",
   /**cli强行覆盖提示信息*/
   CLI_FORCE_HELP_MESSAGE: "如果项目存在则会直接覆盖",
+  /**cli强行覆盖询问提示信息*/
+  CLI_FORCE_CHOICE_HELP_MESSAGE: "目标目录已存在，是否进行强制覆盖",
+  /**cli强行覆盖中提示信息*/
+  CLI_FORCE_SUCCESS_HELP_MESSAGE: "强制覆盖中...",
   /**项目创建成功后的提示*/
-  CLI_CREATE_SUCCESS_HELP_MESSAGE: "如果项目存在则会直接覆盖",
+  CLI_CREATE_SUCCESS_HELP_MESSAGE: "已创建成功，加油，前端人！",
+  /**项目下载时的配置*/
+  CLI_DOWNLOADING_HELP_MESSAGE: "远程模版下载中...",
+  /**项目下载时的配置*/
+  CLI_DOWNLOADING_ERROR_HELP_MESSAGE:
+    "脚手架创建失败, 请尝试在终端中运行frontEnd --help 寻求帮助！",
 };
