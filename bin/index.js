b86aefee3a738d240935c031a0106266ed40067e
aoda <aoda.q.zhang@newegg.com>
2022-04-08 12:38:57 +0800

    更新部分提示语

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,46 @@
 #! /usr/bin/env node
 
 // 命令执行（必选）
-const program = require('commander')
+const program = require("commander");
 // 特定图形绘制（可选）
-const figlet = require('figlet')
+const figlet = require("figlet");
 // 特定样式命令行（可选，注意版本问题）
-const chalk = require('chalk')
-// 获取package.json 特定信息
-const packageJson=require('../package.json')
-
+const chalk = require("chalk");
+// 自定义配置文件
+const config = require("../config/index");
 
 program
   // 自定义定义命令和参数
-  .command('create <项目名称>')
-  .description('创建一个新的项目')
+  .command("create <项目名称>")
+  .description(config?.CLI_CREATE_HELP_MESSAGE)
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option('-f, --force', '如果项目存在则会直接覆盖')
+  .option("-f, --force", config?.CLI_FORCE_HELP_MESSAGE)
   .action((name, options) => {
     // 执行具体的项目创建动作
-    require('../lib/index.js')(name, options)
-  })
-  
-program
-   // 配置版本号信息
-  .version(`v${require('../package.json').version}`)
-  .usage('<command> [option]')
+    require("../lib/index.js")(name, options);
+  });
+
+// program
+//    // 配置版本号信息
+//   .version(`v${require('../package.json').version}`)
+//   .usage('<command> [option]')
 
 // 帮助命令
-  program
-  .on('--help', () => {
-    // 使用 figlet 绘制 脚手架Logo
-    console.log('\r\n' + figlet.textSync('aoda', {
-      font: 'Ghost',
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-      width: 80,
-      whitespaceBreak: true
-    }));
-    // 新增说明信息
-    console.log(chalk.green('抱歉未能给您解决脚手架带来的问题，如需帮助，请联系我，电话 19102632826，base：成都'));
-  })
-  
+program.on("--help", () => {
+  // 使用 figlet 绘制 脚手架Logo
+  console.log(
+    "\r\n" +
+      figlet.textSync(config?.CURRENT_CLI_LOGONAME, {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+      })
+  );
+  // 帮助说明提示
+  console.log(chalk.green(config?.CLI_HELP_MESSAGE));
+});
+
 // 解析用户执行的命令
 program.parse(process.argv);
--- config/index.js
+++ config/index.js
@@ -1,11 +1,22 @@
-module.exports={
-    // TODO 完善提示信息
-    // TODO 完善远端文件过滤
-    // TODO 完善流程逻辑，询问用户 具体的方向 node后端，web前端
-    // TODO config文件抽离
-    // TODO 完善工程化（请求封装，proxy代理设置，路由配置，webpack扩展配置，多语言扩展）
-    /**脚手架当前支持创建的项目技术 */ 
-    CURRENT_SUPPORT_KEY:[ 'vue-template', 'vue3.0-template' ],
-    /**当前采用的git用户账号*/ 
-    CURRENT_GIT_USER:'aodagit',
-}
+module.exports = {
+  // TODO 完善提示信息
+  // TODO 完善远端文件过滤
+  // TODO 完善流程逻辑，询问用户 具体的方向 node后端，web前端
+  // TODO config文件抽离
+  // TODO 完善工程化（请求封装，proxy代理设置，路由配置，webpack扩展配置，多语言扩展）
+  /**脚手架当前支持创建的项目技术 */
+  CURRENT_SUPPORT_KEY: ["vue-template", "vue3.0-template"],
+  /**当前采用的git用户账号*/
+  CURRENT_GIT_USER: "aodagit",
+  /**当前生成的cli自定义logo名*/
+  CURRENT_CLI_LOGONAME: "aoda",
+  /**cli帮助提示信息*/
+  CLI_HELP_MESSAGE:
+    "抱歉未能给您解决脚手架带来的问题，如需帮助，请联系我，电话 19102632826，base：成都",
+  /**cli创建提示信息*/
+  CLI_CREATE_HELP_MESSAGE: "创建一个新的项目",
+  /**cli强行覆盖提示信息*/
+  CLI_FORCE_HELP_MESSAGE: "如果项目存在则会直接覆盖",
+  /**项目创建成功后的提示*/
+  CLI_CREATE_SUCCESS_HELP_MESSAGE: "如果项目存在则会直接覆盖",
+};
