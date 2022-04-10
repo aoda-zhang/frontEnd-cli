b7b08065a231d550d5569c44dd253ee874cd3c0a
aoda <aoda2826@qq.com>
2022-04-10 10:21:55 +0800

    update

--- config/index.js
+++ config/index.js
@@ -33,5 +33,6 @@ module.exports = {
     "脚手架创建失败, 请尝试在终端中运行frontEnd --help 寻求帮助！",
   /**获取个人项目仓库url*/
   // CLI_GETREPO_URL: `https://gitee.com/api/v5/orgs/${this?.CURRENT_GIT_USER}/repos`,
-  CLI_GETREPO_URL: `https://api.github.com/orgs/aodaGit/repos`,
+  // CLI_GETREPO_URL: "https://api.github.com/orgs/aodaGit/repos",
+  CLI_GETREPO_URL: "随机测试",
 };
